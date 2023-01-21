ba62abccb8176373d5cca273ff3d67a4d3396fa1
Suhail Osman <35406282+sahlowle@users.noreply.github.com>
2023-01-21 11:27:00 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\AppointmentController;
-use Cviebrock\EloquentSluggable\Services\SlugService;
+// use Cviebrock\EloquentSluggable\Services\SlugService;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\MeetingController;
 use App\Mail\NewMeeting;
 use App\Models\Meeting;
 use Illuminate\Http\Request;
-use Illuminate\Routing\RouteGroup;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
@@ -17,9 +16,6 @@ use Illuminate\Support\Facades\Route;
 | Auth Routes
 |--------------------------------------------------------------------------
 */
-
-// Route::post('/register', [AuthController::class, 'register']);
-// Route::post('/login', [AuthController::class, 'login']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout']);
@@ -34,7 +30,9 @@ Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     // Appointments Routes
-    Route::apiResource('appointments', AppointmentController::class);
+    Route::apiResource('appointments', AppointmentController::class)->only([
+        'index', 'store'
+    ]);
     // Meeting Scheduled Routs
     Route::get('/meetings/scheduled', [MeetingController::class, 'scheduled']);
 
@@ -45,12 +43,7 @@ Route::post('/meeting', [MeetingController::class, 'store']);
 
 Route::get('/appointments/{username}/{slug}', [AppointmentController::class, 'getBookData']);
 
-Route::get('check-slug', function () {
 
-    $slug = SlugService::createSlug(App\Models\Appointment::class, 'slug', request()->event_name);
-
-    return response()->json(['slug' => $slug]);
-});
 
 
 // get authenticated user
@@ -63,4 +56,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     Mail::to("sahlowle@gmail.com")->later(now()->addMinutes(1),new NewMeeting(Meeting::latest('id')->first()));
 
 //     return "success";
+// });
+
+// Route::get('check-slug', function () {
+
+//     $slug = SlugService::createSlug(App\Models\Appointment::class, 'slug', request()->event_name);
+
+//     return response()->json(['slug' => $slug]);
 // });
